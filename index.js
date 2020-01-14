e46d73bb099e27b6b6a324ac58a746e893b2adfc
Warren Yao <warrenyca905@gmail.com>
2020-01-14 03:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Code your solutions in this file
 function writeCards(names, name){
-  let messages = []
-  for(let i = 0; i < names.length; i++){
-    messages.push(`Thank you, ${names[i]}, for the wonderful ${name} gift!`)
+  let messages = [];
+  for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${name} gift!`);
   }
-  return messages
+  return messages;
 }
 
 function countDown(num){
   let i = num;
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
 }
