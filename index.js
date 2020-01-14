613f46ce0ab8814edc4161b545ab270775982a84
Warren Yao <warrenyca905@gmail.com>
2020-01-14 03:19:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, name){
+  let messages = [];
+  for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${name} gift!`);
+  }
+  return messages;
+}
+
+function countDown(num){
+  let i = num;
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
+}
