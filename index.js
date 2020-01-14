cd7735b8c20fb899e827034a760923616e8774ca
Warren Yao <warrenyca905@gmail.com>
2020-01-14 03:14:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-function wrapCards(names, name){
+function writeCards(names, name){
   let messages = []
   for(let i = 0; i < names.length; i++){
     messages.push(`Thank you, ${names[i]}, for the wonderful ${name} gift!`)
